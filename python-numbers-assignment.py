d5e00d49c6f624e5e50ccad3a3691a1808d2f56d
UVaraprasad <167415681+UVaraprasad@users.noreply.github.com>
2024-06-09 14:55:40 +0530

    2nd Assignment

---
+++ python-numbers-assignment.py
@@ -0,0 +1,18 @@
+i=75
+PI=3.14
+print(i,type(i))
+print(PI,type(PI))
+
+a=4+5j
+b=6+7J
+print(a,type(a))
+print(b,type(b))
+
+
+import sys
+c=123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+print(c)
+print("size :",sys.getsizeof(c))
+print(type(c))
+
+
---
+++ strings-assignment.py
@@ -0,0 +1,9 @@
+s1="Semantic segmentation of Lung Cancer from Chest X-Ray"
+print(s1[48])
+
+s2="I have to Love Python Programming"
+print(s2[-23])
+
+s3='Prasad'
+s3[0]='p'   #string is immutable get error
+print(s3) 
